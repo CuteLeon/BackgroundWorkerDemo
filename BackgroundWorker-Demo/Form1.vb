510e764cb771944ab4e227524a51f091cbc8f9f6
Leon <xiaoyankeji@hotmail.com>
2016-11-15 20:43:51 +0800

    第一次提交

---
+++ BackgroundWorker-Demo/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim BGWDemo As BackgroundWorker
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        BGWDemo = New BackgroundWorker()
+        BGWDemo.WorkerReportsProgress = True
+        BGWDemo.WorkerSupportsCancellation = True
+
+        AddHandler BGWDemo.DoWork, AddressOf DoWork
+        AddHandler BGWDemo.ProgressChanged, AddressOf ProgressChanged
+        AddHandler BGWDemo.RunWorkerCompleted, AddressOf RunWorkerCompleted
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BGWDemo.RunWorkerAsync(100)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        BGWDemo.CancelAsync()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        '后台任务正在执行时需要先取消任务
+        If BGWDemo.IsBusy Then BGWDemo.CancelAsync()
+        BGWDemo.Dispose()
+    End Sub
+
+    ''' <summary>
+    ''' 异步任务
+    ''' </summary>
+    Private Sub DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs)
+        'e.Cancel = False
+        '不要在这里操作前台控件，因为代码运行在另一个线程了
+        Dim BGWIns As BackgroundWorker = CType(sender, BackgroundWorker)
+        Dim Count As Integer = 0
+        For Index As Integer = 0 To 100
+            Count += 1
+            '在这里触发进度更新事件（userState可以帮助传出任意类型的数据）
+            BGWIns.ReportProgress(Index, "任务完成了 " & Index & "%")
+            '使用用户传入的参数当做等待超时
+            Threading.Thread.Sleep(CInt(e.Argument))
+
+            '需要手动检测是否取消任务
+            If BGWIns.CancellationPending Then
+                e.Result = "取消了任务"
+                Exit Sub
+            End If
+        Next
+
+        '任务完成，返回执行结果（Result 可以帮助传出任何类型的数据）
+        e.Result = "任务结束"
+    End Sub
+
+    ''' <summary>
+    ''' 任务进度更新
+    ''' </summary>
+    Private Sub ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs)
+        'WorkLabel.Text = e.ProgressPercentage & "%"
+        WorkLabel.Text = e.UserState.ToString
+    End Sub
+
+    ''' <summary>
+    ''' 任务完成
+    ''' </summary>
+    Private Sub RunWorkerCompleted(ender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs)
+        If e.Cancelled Then WorkLabel.Text = e.Result : Exit Sub
+        If e.Error IsNot Nothing Then WorkLabel.Text = "任务出错：" & e.Error.HResult & vbCrLf & e.Error.Message : Exit Sub
+
+        WorkLabel.Text = e.Result.ToString
+    End Sub
+
+End Class
---
+++ BackgroundWorker-Demo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("BackgroundWorker-Demo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BackgroundWorker-Demo")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("5ac0092d-43a8-4db7-8e91-ed992c900c63")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
